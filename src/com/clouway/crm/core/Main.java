c9021d4fdcf7a238ff7b8ba6b2cc9703ec3e0e26
tsbonev <tsbonev@gmail.com>
2018-04-26 11:49:53 +0300

    Remove commented code and add printl stream
    Removes the PageBean commented out tests and adds a stream of the entrySet
    from the wordCount returned map in order to show that it works

--- src/com/clouway/crm/core/Main.java
+++ src/com/clouway/crm/core/Main.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -17,6 +18,10 @@ public class Main {
 
         String words1 = "aa cc aa ff gg hh ff";
         String words2 = "Ники Илиев Вълчев Вевов";
+
+
+        Stream.of(mostCommon.wordCount(words1).entrySet())
+                .forEach(c -> System.out.println(c));
 
         System.out.println(mostCommon.letterCount(words2));
 
